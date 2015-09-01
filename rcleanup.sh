45852cab3c2d5feb14cc340318a067d73636e69c
tkessler45 <tkessler45@users.noreply.github.com>
2015-09-01 13:38:28 -0700

    Create rcleanup.sh

---
+++ rcleanup.sh
@@ -0,0 +1,9 @@
+# remove old backups (7 days old and older, for a week)
+
+#rm -rf //Burnsxs/rsyncbackup/$USERNAME/`date -v-7d +%A%Y%m%d`
+#rm -rf //Burnsxs/rsyncbackup/$USERNAME/`date -v-8d +%A%Y%m%d`
+#rm -rf //Burnsxs/rsyncbackup/$USERNAME/`date -v-9d +%A%Y%m%d`
+#rm -rf //Burnsxs/rsyncbackup/$USERNAME/`date -v-10d +%A%Y%m%d`
+#rm -rf //Burnsxs/rsyncbackup/$USERNAME/`date -v-11d +%A%Y%m%d`
+#rm -rf //Burnsxs/rsyncbackup/$USERNAME/`date -v-12d +%A%Y%m%d`
+#rm -rf //Burnsxs/rsyncbackup/$USERNAME/`date -v-13d +%A%Y%m%d`
